7cac8ca98ac3e80333c7a956b71540e0f7839b6f
mikeboafo <mikeboafo30@gmail.com>
2026-09-14 12:08:17 +0000

    Restore bundled favicon with branding

--- db/migration_v3_restore_bundled_branding.sql
+++ db/migration_v3_restore_bundled_branding.sql
@@ -2,3 +2,7 @@
 UPDATE site_settings
 SET setting_value = '/assets/logo.svg'
 WHERE setting_key = 'logo_path';
+
+UPDATE site_settings
+SET setting_value = '/assets/favicon.svg'
+WHERE setting_key = 'favicon_path';
